f1ae0035d4e4a7c21830b194959e9f28c9b9dbac
architjain-12 <architjain08@gmail.com>
2021-11-07 18:03:07 +0530

    Added Rating component

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -2,15 +2,15 @@ const products = [
   {
     _id: "1",
     name: "Airpods Wireless Bluetooth Headphones",
-    image: "/images/airpods.jpg",
+    image: "/public/images/airpods.jpg",
     description:
       "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
     brand: "Apple",
     category: "Electronics",
-    price: 8999,
+    price: 18999,
     countInStock: 10,
     rating: 4.5,
-    numReviews: 12,
+    numReviews: 4,
   },
   {
     _id: "2",
